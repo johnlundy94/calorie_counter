935fbbce1aa4bf1e685847ec3383ba1f03e7f3d6
John Lundy <johnclundyy@gmail.com>
2023-09-08 12:00:30 -0600

    Validation for signing in and up complete

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,56 +1,62 @@
-import "../../styling/SignIn.css"
-import React, { useContext } from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { UserContext } from '../../context/userContext';
+import "../../styling/SignIn.css";
+import React, { useState, useContext } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { UserContext } from "../../context/userContext";
 import { getDoc, doc } from "firebase/firestore";
-import firebaseConfig from '../../config/firebase';
-import { useNavigate } from 'react-router-dom';
+import firebaseConfig from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
-const { db } = firebaseConfig; 
+const { db } = firebaseConfig;
 
 const SignIn = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  const {userDispatch} = useContext(UserContext);
+  const { userDispatch } = useContext(UserContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setErrorMessage("");
     const email = e.target.email.value;
     const password = e.target.password.value;
 
     const auth = getAuth();
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       const userDoc = await getDoc(doc(db, "users", user.uid));
 
       if (userDoc.exists()) {
         const userData = userDoc.data();
-        
+
         userDispatch({
-          type: 'SET_USER_DATA',
+          type: "SET_USER_DATA",
           payload: userData,
         });
       }
 
       userDispatch({
-        type: 'SET_UID',
+        type: "SET_UID",
         payload: user.uid,
       });
 
-      console.log('Signed in:', user);
-      navigate('/calorie-counter')
+      navigate("/calorie-counter");
     } catch (error) {
-      console.error('Error in sign in:', error);
-      window.alert('Error in sign in:', error)
+      console.error("Error in sign in:", error);
+      setErrorMessage("Incorrect email or password");
     }
-  }
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <input name='email' type='email' placeholder='Email' required/>
-      <input name='password' type='password' placeholder='Password' required/>
+      <input name="email" type="email" placeholder="Email" required />
+      <input name="password" type="password" placeholder="Password" required />
       <button type="submit">Sign In</button>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
     </form>
   );
 };
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,48 +1,90 @@
-import "../../styling/SignUp.css"
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import firebaseConfig from '../../config/firebase';
-import { UserContext } from '../../context/userContext';
-import {doc, setDoc} from "firebase/firestore";
+import "../../styling/SignUp.css";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import firebaseConfig from "../../config/firebase";
+import { UserContext } from "../../context/userContext";
+import { doc, setDoc } from "firebase/firestore";
 
 const { db, auth } = firebaseConfig;
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const {userDispatch} = useContext(UserContext)
+  const { userDispatch } = useContext(UserContext);
+
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
 
+    if (!emailValid || !passwordValid) {
+      alert("Please correct the errors before submitting.");
+      return;
+    }
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
       const user = userCredential.user;
 
       await setDoc(doc(db, "users", user.uid), {
         tdee: 0,
-      })
+      });
 
       userDispatch({
-        type: 'SET_UID',
+        type: "SET_UID",
         payload: user.uid,
       });
 
-      console.log('Account created:', user);
-      navigate('/questions')
+      console.log("Account created:", user);
+      navigate("/questions");
     } catch (error) {
-      console.error('Error in signup:', error);
-      window.alert('Error in signup:', error)
+      console.error("Error in signup:", error);
+      window.alert("Error in signup:", error);
     }
+  };
+
+  const handleEmailChange = (e) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    setEmailValid(emailRegex.test(e.target.value));
+  };
+
+  const handlePasswordChange = (e) => {
+    setPasswordValid(e.target.value.length >= 8);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name='email' type='email' placeholder='Email' required/>
-      <input name='password' type='password' placeholder='Password' required/>
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        required
+        onChange={handleEmailChange}
+        className={emailValid ? "" : "invalid-input"}
+      />
+      {!emailValid && (
+        <p className="error-text">Please enter a valid email address.</p>
+      )}
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        required
+        minLength="8"
+        onChange={handlePasswordChange}
+        className={passwordValid ? "" : "invalid-input"}
+      />
+      {!passwordValid && (
+        <p className="error-text">Password must be at least 8 characters.</p>
+      )}
+
       <button type="submit">Sign Up</button>
     </form>
   );
